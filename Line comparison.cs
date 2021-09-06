5786e3b697d94d5c15f0d0863a49c6d45701599d
unknown <shaunak12wankhade@ggmail.com>
2021-09-06 13:29:20 +0530

    [Shaunak] Added calculation of single line length

---
+++ Line comparison.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_comparison1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison");
+            //UC_1_calculating single line length
+            double x1, x2, y1, y2;
+            Console.WriteLine("Enter co-ordinates (x1, y1): ");
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinates (x2, y2): ");
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The Length of the line is: " + length);
+        }
+    }
+}
